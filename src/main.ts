24f6710bf9d1fbb6c6a8215eebaef8bdc9812f72
吴志强 <1076976262@qq.com>
2023-04-12 15:15:26 +0800

    feat: 🎸 添加第三方库引入管理，添加utils工具类

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import router from './router'
 // 配置ant-design-vue
 import 'ant-design-vue/dist/antd.css';
 import antd from 'ant-design-vue';
-import FastTable from 'antdv-fast-table';
 
 // 配置vuex
 import vuex from "@/store/index"
@@ -13,10 +12,15 @@ import vuex from "@/store/index"
 // 多语言
 import i18n from './i18n'
 
+// axios 网络请求
+
+// 三方库注入
+import PublicInjector from '@/public-injector'
+
 createApp(App)
 .use(antd)
 .use(router)
 .use(vuex)
 .use(i18n)
-.use(FastTable)
+.use(PublicInjector)
 .mount('#app')
